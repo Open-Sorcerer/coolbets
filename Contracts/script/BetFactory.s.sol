3dd0bc486f47726c1a9751634fffb718a6098f32
Aayush Gupta <guptaaayush432@gmail.com>
2024-08-25 19:25:05 +0530

    created BetFactory.s.sol

---
+++ Contracts/script/BetFactory.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Script.sol";
+import {BetFactory} from "../src/BetFactory.sol";
+
+contract DeployBetFactory is Script {
+    function run() external {
+        // Retrieve the private key from the environment variable
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        // Start broadcasting transactions
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy the BetFactory contract
+        BetFactory betFactory = new BetFactory();
+
+        // Stop broadcasting transactions
+        vm.stopBroadcast();
+
+        // Log the address of the deployed contract
+        console.log("BetFactory deployed at:", address(betFactory));
+    }
+}
+
+// forge script script/Counter.s.sol --rpc-url $ROOTSTOCK_RPC_URL --broadcast --legacy
+// forge script script/BetFactory.s.sol --rpc-url $ROOTSTOCK_TESTNETWORK_RPC_URL --broadcast --legacy
