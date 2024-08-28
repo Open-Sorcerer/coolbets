954eeba988404824a2f0e0c4fb6c5998b16a2603
Aayush Gupta <guptaaayush432@gmail.com>
2024-08-28 18:45:51 +0530

    Created deploy script for CoolBets

---
+++ Contracts/script/CoolBets.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Script.sol";
+import {CoolBets} from "../src/CoolBets.sol";
+
+contract DeployCoolBets is Script {
+    function run() external {
+        // Retrieve the private key from the environment variable
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        // Start broadcasting transactions
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy the CoolBets contract
+        CoolBets coolBets = new CoolBets();
+
+        // Stop broadcasting transactions
+        vm.stopBroadcast();
+
+        // Log the address of the deployed contract
+        console.log("CoolBets deployed at:", address(coolBets));
+    }
+}
+
+// forge script script/CoolBets.s.sol --rpc-url $ROOTSTOCK_TESTNETWORK_RPC_URL --broadcast --legacy
+// forge script script/CoolBets.s.sol --rpc-url $CHILIZ_TESTNETWORK_RPC_URL --broadcast
+// forge script script/CoolBets.s.sol --rpc-url $MORPHL2_HOLESKY_RPC_URL --broadcast --legacy
