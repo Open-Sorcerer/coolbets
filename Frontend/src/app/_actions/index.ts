54357fd5073ebcba6c60abaed991f80de3e821d3
rkmonarch <rahulkulkarni5234@gmail.com>
2024-09-07 23:43:52 +0530

    feat: added indexing queries

--- Frontend/src/app/_actions/index.ts
+++ Frontend/src/app/_actions/index.ts
@@ -17,3 +17,72 @@ export const getProposals = async () => {
   const data = await graphResponse.json();
   return data;
 };
+
+export const getProposalsFinalized = async () => {
+  const graphResponse = await fetch("https://indexer.bigdevenergy.link/8c40956/v1/graphql", {
+    method: "POST",
+    body: JSON.stringify({
+      query: `
+        query MyQuery {
+          CoolBets_ProposalFinalized {
+            id
+          }
+        }
+      `,
+    }),
+  });
+  const data = await graphResponse.json();
+  return data;
+};
+
+export const getRewardsDistributed = async () => {
+  const graphResponse = await fetch("https://indexer.bigdevenergy.link/8c40956/v1/graphql", {
+    method: "POST",
+    body: JSON.stringify({
+      query: `
+        query MyQuery {
+          CoolBets_RewardsDistributed {
+            id
+          }
+        }
+      `,
+    }),
+  });
+  const data = await graphResponse.json();
+  return data;
+};
+
+export const getVotesPlaced = async () => {
+  const graphResponse = await fetch("https://indexer.bigdevenergy.link/8c40956/v1/graphql", {
+    method: "POST",
+    body: JSON.stringify({
+      query: `
+        query MyQuery {
+  CoolBets_VotePlaced {
+            id
+            }
+        }
+      `,
+    }),
+  });
+  const data = await graphResponse.json();
+  return data;
+};
+
+export const getRewardsClaimed = async () => {
+  const graphResponse = await fetch("https://indexer.bigdevenergy.link/8c40956/v1/graphql", {
+    method: "POST",
+    body: JSON.stringify({
+      query: `
+        query MyQuery {
+          CoolBets_RewardReceivedUser {
+            id
+            }
+            }
+        }
+      `,
+    }),
+  });
+  const data = await graphResponse.json();
+  return data;
+};
